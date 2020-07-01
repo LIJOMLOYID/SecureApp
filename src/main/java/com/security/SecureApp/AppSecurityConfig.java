75fcdc22d4c97579af5f75910d6c2b0fbacc7f9c
Lijo M Loyid <lijomloyid@gmail.com>
2020-07-01 01:00:31 -0700

    Connected MySQL

--- src/main/java/com/security/SecureApp/AppSecurityConfig.java
+++ src/main/java/com/security/SecureApp/AppSecurityConfig.java
@@ -2,29 +2,45 @@ package com.security.SecureApp;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Bean
-	@Override
-	protected UserDetailsService userDetailsService() {
-		List <UserDetails> users = new ArrayList<>();
-		users.add(User.withDefaultPasswordEncoder()
-				.username("lijo")
-				.password("1234")
-				.roles("USER")
-				.build());
-		return new InMemoryUserDetailsManager(users);
+	public AuthenticationProvider authProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		return provider;
+	
 	}
+//	@Bean
+//	@Override
+//	protected UserDetailsService userDetailsService() {
+//		List <UserDetails> users = new ArrayList<>();
+//		users.add(User.withDefaultPasswordEncoder()
+//				.username("lijo")
+//				.password("1234")
+//				.roles("USER")
+//				.build());
+//		return new InMemoryUserDetailsManager(users);
+//	}
 
 }
